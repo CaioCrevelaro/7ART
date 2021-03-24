09edf0226daf14731ed72591ac37132bf74aebe2
CaioCrevelaro <caio.m.b.crevelaro@gmail.com>
2021-03-24 19:15:22 -0300

    Intents feitas

--- app/src/main/java/com/br/seventh_art/view/LoginActivity.kt
+++ app/src/main/java/com/br/seventh_art/view/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.br.seventh_art.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.br.seventh_art.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val sigInButton by lazy { findViewById<Button>(R.id.button_signin) }
+
+        sigInButton.setOnClickListener {
+
+            val intent = Intent(it.context, CategoryContentActivity::class.java)
+            it.context.startActivity(intent)
+        }
     }
 }
