614f8bed9300c45840b7c8638ac6a57c90f0d801
Lais Rolim Yonaha <laisrolimyonaha@users.noreply.github.com>
2021-03-24 21:24:12 -0300

    Layout SignIn/SignUp

--- app/src/main/java/com/br/seventh_art/model/CategoryContent.kt
+++
@@ -1,8 +0,0 @@
-package com.br.seventh_art.model
-
- data class CategoryContent (
-     val poster: Int,
-     val categoryContent: String
-         )
-
-
--- app/src/main/java/com/br/seventh_art/view/LoginActivity.kt
+++ app/src/main/java/com/br/seventh_art/view/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.br.seventh_art.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.br.seventh_art.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signInButton by lazy { findViewById<Button>(R.id.button_signin_login) }
+        val signUPButton by lazy { findViewById<Button>(R.id.button_signup_login) }
+
+        signInButton.setOnClickListener {
+            val intent = Intent(it.context, SignInActivity::class.java)
+            it.context.startActivity(intent)
+        }
+
+        signUPButton.setOnClickListener {
+            val intent = Intent(it.context, SignUpActivity::class.java)
+            it.context.startActivity(intent)
+        }
+
     }
 }
